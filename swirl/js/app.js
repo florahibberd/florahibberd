35c3d12c26320ef63c7992d029a115f349e29a7f
florahibberd <80952579+florahibberd@users.noreply.github.com>
2024-06-17 19:27:17 +0200

    Update app.js

--- swirl/js/app.js
+++ swirl/js/app.js
@@ -993,3 +993,24 @@ videoGallery.addEventListener("mouseover", function() {
 videoGallery.addEventListener("mouseout", function() {
   circleCursor.style.backgroundColor = "rgba(0, 0, 0, 0.712)";
 });
+
+// READ MORE
+
+function changeReadMore() { 
+    const mycontent = 
+        document.getElementById('mybox1id'); 
+    const mybutton = 
+        document.getElementById('mybuttonid'); 
+    const span1 = document.getElementById("span1") 
+  
+    if (mycontent.style.display === 'none'
+        || mycontent.style.display === '') { 
+        mycontent.style.display = 'inline'; 
+        span1.style.display = "none"; 
+        mybutton.textContent = 'Read Less'; 
+    } else { 
+        mycontent.style.display = 'none'; 
+        mybutton.textContent = 'Read More'; 
+        span1.style.display = "inline"; 
+    } 
+} 
