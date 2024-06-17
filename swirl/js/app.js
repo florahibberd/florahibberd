356d46ee537430960acd7396e299496513c68b98
florahibberd <80952579+florahibberd@users.noreply.github.com>
2024-06-17 19:47:07 +0200

    Update app.js

--- swirl/js/app.js
+++ swirl/js/app.js
@@ -1,3 +1,21 @@
+/// READ MORE 
+
+function myFunction() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("myBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
+}
+
 // ! ************ VARIABLES  ********
 var mainWrapper = document.getElementById('mainWrapper');
 // ?slidebar VARIABLES
