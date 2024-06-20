c8fb0318e88fad8c7b815058e8094e147ab99337
florahibberd <80952579+florahibberd@users.noreply.github.com>
2024-06-20 17:58:14 +0200

    Update app.js

--- swirl/js/app.js
+++ swirl/js/app.js
@@ -16,8 +16,7 @@ function changeReadMore() {
         mycontent.style.display = 'none'; 
         mybutton.textContent = 'Read More'; 
         span1.style.display = "inline";
-        window.scrollTo({ top: 800, behavior: 'smooth' });
-        // window.location.hash = '#mainBio' behavior: 'smooth' 
+        window.scrollTo({ top: 650, behavior: 'smooth' });
         
     } 
 } 
